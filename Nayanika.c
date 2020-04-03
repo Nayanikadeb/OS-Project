17f3a7679d4c33dd21aff22d5a5433fad934f091
Nayanikadeb <47624713+Nayanikadeb@users.noreply.github.com>
2020-04-03 14:01:04 +0530

    Update Nayanika.c

--- Nayanika.c
+++ Nayanika.c
@@ -10,35 +10,28 @@ int b;
 pid_t pid;
 printf("Enter the  number\n");
 scanf("%d",&b);
-if(b<0)
+do{
+if(b<=0)
 printf("Invalid Input. Please enter a positive number");
+}while(b<=0);
 pid=fork();
 if(pid==0)
 {
 printf("\nChild process is working \n");
+ printf("%d \n",b);
 while(b!=1)
 {
         if(b%2==0)
         b=b/2;
         else if(b%2==1)
         b=3*(b)+1;
-printf("%d \n",b);
-printf("Invalid Input. Please enter a positive number");
-pid=fork();
-if(pid==0)
-{
-printf("\n Child process is working \n");
-while(b!=1)
-{
-        if(b%2==0)
-        b=b/2;
-        else if(b%2==1)
-        b=3*(b)+1;
+
 printf("%d \n",b);
 
-}                            
+}                        
 printf("\n Child Process is completed\n");
 }
+
 else
 {
 printf("\nParent process waiting for child process\n");
